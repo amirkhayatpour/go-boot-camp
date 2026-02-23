90019cd7e90e7fee7fd7c47f8576cb74e8d56d0c
Amir <amirkhayatpour@gmail.com>
2026-02-23 16:31:01 +0330

    add secound user to passme

--- passme/main.go
+++ passme/main.go
@@ -12,17 +12,32 @@ func main() {
 		return
 	}
 
-	name := "amir"
-	password := "1234"
+	const (
+		jackName               = "jack"
+		jackPassword           = "1888"
+		inancName              = "inanc"
+		inancPassword          = "1879"
+		invalidUsernameMessage = "Access denied for %q\n"
+		invalidPasswordMessage = "Invalid Password for %q\n"
+		accessGrantMessage     = "Access granted to %q\n"
+	)
 
-	inputName := os.Args[1]
-	inputPassword := os.Args[2]
+	username := os.Args[1]
+	password := os.Args[2]
 
-	if inputName != name {
-		fmt.Printf("Access denied for \"%s\"", inputName)
-	} else if inputName == name && inputPassword != password {
-		fmt.Println("Invalid password for \"amir\"")
-	} else if inputName == name && inputPassword == password {
-		fmt.Println("Access granted to \"amir\"")
+	if username == jackName {
+		if password == jackPassword {
+			fmt.Printf(accessGrantMessage, jackName)
+		} else {
+			fmt.Printf(invalidPasswordMessage, jackName)
+		}
+	} else if username == inancName {
+		if password == inancPassword {
+			fmt.Printf(accessGrantMessage, inancName)
+		} else {
+			fmt.Printf(invalidPasswordMessage, inancName)
+		}
+	} else {
+		fmt.Printf(invalidUsernameMessage, username)
 	}
 }
